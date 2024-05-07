504c95b9c425fa62712aa97e915f11bd91687de3
Feeergus <jero.sesto@gmail.com>
2024-05-07 18:35:57 -0300

    todo limpio

---
+++ scenes/Game.js
@@ -0,0 +1,23 @@
+// URL to explain PHASER scene: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/scene/
+
+export default class Game extends Phaser.Scene {
+  constructor() {
+    super("main");
+  }
+
+  init() {
+    
+  }
+
+  preload() {
+    
+  }
+
+  create() {
+
+  }
+
+  update() {
+ 
+  }
+}
